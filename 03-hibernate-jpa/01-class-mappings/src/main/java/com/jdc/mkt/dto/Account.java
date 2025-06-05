cb0d0a0bbe01d4efe038198caa4ede354b32cfcd
uthuyein <uthuyeinlinn@gmail.com>
2025-06-05 12:16:29 +0630

    update

--- 03-hibernate-jpa/01-class-mappings/src/main/java/com/jdc/mkt/dto/Account.java
+++ 03-hibernate-jpa/01-class-mappings/src/main/java/com/jdc/mkt/dto/Account.java
@@ -1,23 +1,34 @@
 package com.jdc.mkt.dto;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Index;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
+@Table(
+		name = "account_tbl",
+		indexes = {
+				@Index(columnList = "username,email")
+		},
+		uniqueConstraints = {
+				@UniqueConstraint(columnNames = {"username","email"})
+		}
+		)
 public class Account {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
-	@Column(nullable = false,unique = true)
 	private String username;
+	private String email;
 	private String password;
 	private boolean active;
 }
