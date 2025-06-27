ea3217521107d6c27b4ee15dfee368fb21909769
uthuyein <uthuyeinlinn@gmail.com>
2025-06-27 09:39:01 +0630

    udpate

--- 03-hibernate-jpa/07-query/src/main/resources/META-INF/import.sql
+++ 03-hibernate-jpa/07-query/src/main/resources/META-INF/import.sql
@@ -11,5 +11,13 @@ insert into addresses_tbl(state,township,street) values ('Yangon','Yankin','no12
 insert into addresses_tbl(state,township,street) values ('Yangon','Sanchaung','n0122 second floor');
 
 insert into licences_tbl(id,licenceNumber,licenceType,bloodType,dob,issueDate,validDate)values(1,'AA34333','C','A','1990-10-11','2006-11-21','2011-10-12');
+insert into licences_tbl(id,licenceNumber,licenceType,bloodType,dob,issueDate,validDate)values(2,'AB43222','C','B','1991-05-21','2008-11-21','2011-10-12');
+insert into licences_tbl(id,licenceNumber,licenceType,bloodType,dob,issueDate,validDate)values(3,'AA21212','D','AB','1990-10-04','2009-01-21','2014-01-21');
+insert into licences_tbl(id,licenceNumber,licenceType,bloodType,dob,issueDate,validDate)values(4,'AC32323','D','O','1990-07-20','2007-01-11','2012-01-11');
+insert into licences_tbl(id,licenceNumber,licenceType,bloodType,dob,issueDate,validDate)values(5,'BB23234','E','O','1990-10-11','2008-05-21','2013-05-21');
 
 insert into drivers_tbl(id,name,gender,email,phone,addresses_id,licences_id)values(1,'AungKo','MALE','aungko@gmail.com','09222222',1,1);
+insert into drivers_tbl(id,name,gender,email,phone,addresses_id,licences_id)values(2,'MyoThu','MALE','myothu@gmail.com','09343442',2,2);
+insert into drivers_tbl(id,name,gender,email,phone,addresses_id,licences_id)values(3,'SanSanOo','FEMALE','sansan@gmail.com','092121288',3,3);
+insert into drivers_tbl(id,name,gender,email,phone,addresses_id,licences_id)values(4,'Wanna','MALE','wanna@gmail.com','09542342',4,4);
+insert into drivers_tbl(id,name,gender,email,phone,addresses_id,licences_id)values(5,'Zarni','MALE','zarni@gmail.com','0988777434',5,5);
